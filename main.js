2bc0e350e397120de3e574072012df3db17299ee
Daniel Correa <fernandocorrea256@gmail.com>
2023-02-11 23:00:48 -0500

    space out and space in

--- main.js
+++ main.js
@@ -12,6 +12,8 @@ canvas.height = cloth_height;
 let Point = function(x, y) {
     this.x = x
     this.y = y
+    this.x0 = x
+    this.y0 = y
     this.moving = false
 }
 
@@ -23,26 +25,52 @@ Point.prototype.draw = function(color="#494b4d") {
 }
 
 Point.prototype.spaceOut = function({x, y}) {
+    if (this.moving) clearInterval(this.animationId)
+
     let adjacent = Math.abs(x - this.x)
     let opposite = Math.abs(y - this.y)
 
     let h = Math.sqrt((adjacent*adjacent) + (opposite*opposite))
 
-    let angle = Math.asin(adjacent/h)
-    let new_length = h*1.5
+    let angle = Math.asin(opposite/h)
+    let new_length = h * 2
 
     let delta_x = (Math.cos(angle) * new_length) - adjacent 
     let delta_y = (Math.sin(angle) * new_length) - opposite
-
+    
     if (this.x < x) delta_x *= -1
     if (this.y < y) delta_y *= -1
-
+    
+    this.moving = true
+    
+    let i = 0
     this.animationId = setInterval(() => {
         ctx.clearRect(this.x-5, this.y-5, 10, 10)
-        this.x += (delta_x / 200)
-        this.y += (delta_y / 200)
+        this.x += (delta_x / 10)
+        this.y += (delta_y / 10)
         this.draw()
-    }, 10)
+        if (++i == 10) {
+            clearInterval(this.animationId)
+            this.spaceIn()
+        }
+    }, 25)
+}
+
+Point.prototype.spaceIn = function() {
+    delta_x = this.x0 - this.x 
+    delta_y = this.y0 - this.y
+    console.log("x: "+ this.x + " y: " + this.y + " x0: " + this.x0 + " y0: "+ this.y0 + " delta_x: " + delta_x + " delta_y: " + delta_y)
+    let i = 10
+    this.animationId = setInterval(() => {
+        ctx.clearRect(this.x-5, this.y-5, 10, 10)
+        this.x += (this.x0 - this.x) / i
+        this.y += (this.y0 - this.y) / i
+        this.draw()
+        if(--i == 1) {
+            clearInterval(this.animationId)
+            this.moving = false
+        } 
+    }, 25)
 }
 
 let Cloth = function() {
@@ -77,23 +105,8 @@ window.addEventListener('resize', function() {
 })
 
 canvas.onmousemove = function(e) { 
-    let affected = cloth.points.filter((({x, y, moving}) => Math.abs(e.x - x) < 60 && Math.abs(e.y - y) < 60 && !moving))
-
+    let affected = cloth.points.filter((({x, y}) => Math.abs(e.x - x) < 60 && Math.abs(e.y - y) < 60))
     affected.forEach(p => {
-        ctx.clearRect(p.x-5,p.y-5, 10, 10)
-        let xBefore = p.x
-        let yBefore = p.y
-        p.moving = true
         p.spaceOut(e)
-        //p.draw()
-
-        setTimeout(() => {
-            clearInterval(p.animationId)
-            ctx.clearRect(p.x-5,p.y-5, 15, 15)
-            p.x = xBefore
-            p.y = yBefore
-            p.moving = false
-            p.draw()
-        }, 2000)
     })
 }
